a900ee68abcd039ad056bddb7b8ff1576dc2121e
oldoworldboa <oldworldboa@gmail.com>
2026-03-21 21:49:21 -0500

    Add yay to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-pacman -S --no-confirm vifm
+# Install yay
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+cd ..
+
+# Install yay packages
+yay -S librewolf-bin
+
+# Install pacman packages
+pacman -S --no-confirm vifm kitty waybar
 
 cp -r ./hypr/* ~/.config/hypr/
 cp -r ./nvim/* ~/.config/nvim/
